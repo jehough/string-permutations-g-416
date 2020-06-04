1741db7c91bb14799211960f27b3949bcd062d95
Jim Hough <teachlife@outlook.com>
2020-06-04 04:45:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function findAllPermutations(string){
+  solution = []
+  array = string.split('')
+  
+  for (let letter of array){
+    array = array.filter(el => el !== letter)
+    let index = 0
+    while (index < array.length){
+      let i = array.length
+      array.splice(index, 0, letter)
+      if (!solution.includes(array.join(''))){
+        while(i >= 0){
+          let first = array.shift()
+          array.push(first)
+          solution.push(array.join(''))
+          i -= 1
+        }
+      }
+      array = array.filter(el => el !== letter)
+      index +=1
+    }
+    array.push(letter)
+  }
+  return solution
+}
